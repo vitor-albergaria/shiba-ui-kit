e59077a868c25415724e3b0eee7dee7ffe9862ff
VitorSoer <contato.soer@gmail.com>
2025-04-14 15:08:20 -0300

    📄 docs: add docs to Range component

--- src/components/Range/Range.stories.tsx
+++ src/components/Range/Range.stories.tsx
@@ -1,10 +1,82 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Range } from './Range';
 import { IRange } from './Range.types';
+import { t } from 'i18next';
+import { DOCS_COLOR_TYPES } from '../../docs/utils/constants';
 
 const meta: Meta<IRange> = {
   title: 'Components/Inputs/Range',
   component: Range,
+  argTypes: {
+    value: {
+      control: 'number',
+      description: t('docs_value'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '0' },
+        category: 'Component',
+      },
+    },
+
+    handleChange: {
+      description: t('docs_click'),
+      table: {
+        type: { summary: '(range: number) => void' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+
+    width: {
+      control: 'number',
+      description: t('docs_type_number'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '200px' },
+        category: 'Component',
+      },
+    },
+
+    height: {
+      control: 'number',
+      description: t('docs_type_number'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '8px' },
+        category: 'Component',
+      },
+    },
+
+    background: {
+      control: 'text',
+      description: t('docs_predefined'),
+      table: {
+        type: { summary: DOCS_COLOR_TYPES },
+        defaultValue: { summary: 'highlight' },
+        category: 'Component',
+      },
+    },
+
+    isFullWidth: {
+      control: 'boolean',
+      description: t('docs_full_width'),
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+        category: 'Component',
+      },
+    },
+
+    isHidden: {
+      control: 'boolean',
+      description: t('docs_hide'),
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+        category: 'Component',
+      },
+    },
+  },
 };
 
 export default meta;
