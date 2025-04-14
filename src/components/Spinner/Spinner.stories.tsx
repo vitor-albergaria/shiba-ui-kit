1e6f27e10cc0d40483a661d4d122ea341af5c640
VitorSoer <contato.soer@gmail.com>
2025-04-14 15:28:50 -0300

    📄 docs: edit docs to Spinner component

--- src/components/Spinner/Spinner.stories.tsx
+++ src/components/Spinner/Spinner.stories.tsx
@@ -1,10 +1,43 @@
+import { t } from 'i18next';
 import { Spinner } from './Spinner';
 import { ISpinner } from './Spinner.types';
 import { Meta, StoryObj } from '@storybook/react';
+import { DOCS_COLOR_TYPES } from '../../docs/utils/constants';
 
 const meta: Meta<ISpinner> = {
   title: 'Components/Feedback/Spinner',
   component: Spinner,
+  argTypes: {
+    size: {
+      control: 'number',
+      description: t('docs_type_number'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '20' },
+        category: 'Component',
+      },
+    },
+    
+    background: {
+      control: 'text',
+      description: t('docs_predefined'),
+      table: {
+        type: { summary: DOCS_COLOR_TYPES },
+        defaultValue: { summary: 'primary' },
+        category: 'Component',
+      },
+    },
+
+    isHidden: {
+      control: 'boolean',
+      description: t('docs_hide'),
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+        category: 'Component',
+      },
+    },
+  },
 };
 
 export default meta;
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './Spinner.module.css';
 import { ISpinner } from './Spinner.types';
+import { handleDynamicStyles } from '../../theme/helpers/handleDinamicStyles';
+import { getThemeColor } from '../../theme/helpers/getTheme';
 
 export const Spinner: React.FC<ISpinner> = ({
   size = 20,
@@ -8,6 +10,12 @@ export const Spinner: React.FC<ISpinner> = ({
   isHidden = false,
 }) => {
   if (isHidden) return null;
+
+  const dynamicStyles = handleDynamicStyles({
+    width: `${size}px`,
+    height: `${size}px`,
+    borderWidth: `${size / 5}px`,
+  });
 
   return (
     <div
@@ -17,11 +25,9 @@ export const Spinner: React.FC<ISpinner> = ({
       data-testid='spinner'
       className={styles.spinner}
       style={{
-        width: `${size}px`,
-        height: `${size}px`,
-        borderWidth: `${size / 5}px`,
-        borderTopColor: `var(--${background})`,
-        borderRightColor: `var(--${background})`,
+        ...dynamicStyles,
+        borderTopColor: getThemeColor(background),
+        borderRightColor: getThemeColor(background),
       }}
     />
   );
