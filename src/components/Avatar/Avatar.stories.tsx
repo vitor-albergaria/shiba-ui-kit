bad2bee376ea9e11b394d949da4cb16669e7a850
VitorSoer <contato.soer@gmail.com>
2025-04-14 13:21:23 -0300

    📄 docs: add docs in Avatar component

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,10 +1,73 @@
+import { t } from 'i18next';
 import { Avatar } from './Avatar';
 import { IAvatar } from './Avatar.types';
 import { Meta, StoryObj } from '@storybook/react';
+import { DOCS_COLOR_TYPES } from '../../docs/utils/constants';
 
 const meta: Meta<IAvatar> = {
   title: 'Components/Feedback/Avatar',
   component: Avatar,
+  argTypes: {
+    image: {
+      control: 'text',
+      description: t('docs_image'),
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+
+    username: {
+      control: 'text',
+      description: t('docs_content'),
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+
+    textColor: {
+      control: 'text',
+      description: t('docs_predefined'),
+      table: {
+        type: { summary: DOCS_COLOR_TYPES },
+        defaultValue: { summary: 'primary' },
+        category: 'Component',
+      },
+    },
+
+    background: {
+      control: 'text',
+      description: t('docs_predefined'),
+      table: {
+        type: { summary: DOCS_COLOR_TYPES },
+        defaultValue: { summary: 'primary' },
+        category: 'Component',
+      },
+    },
+
+    size: {
+      control: { type: 'number' },
+      description: t('docs_type_number'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '50' },
+        category: 'Component',
+      },
+    },
+
+    isHidden: {
+      control: 'boolean',
+      description: t('docs_hide'),
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+        category: 'Component',
+      },
+    },
+  },
 };
 
 export default meta;
