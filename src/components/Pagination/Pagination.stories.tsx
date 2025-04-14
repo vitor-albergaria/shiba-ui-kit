535cdfff886df86afe8712a84324968bd3dbc0df
VitorSoer <contato.soer@gmail.com>
2025-04-14 14:39:50 -0300

    📄 docs: add docs to Pagination component

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,3 +1,4 @@
+import { t } from 'i18next';
 import { Pagination } from './Pagination';
 import { IPagination } from './Pagination.types';
 import { Meta, StoryObj } from '@storybook/react';
@@ -5,6 +6,61 @@ import { Meta, StoryObj } from '@storybook/react';
 const meta: Meta<IPagination> = {
   title: 'Components/Navigation/Pagination',
   component: Pagination,
+  argTypes: {
+    currentPage: {
+      control: 'number',
+      description: t('docs_value'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+    itemsPerPage: {
+      control: 'number',
+      description: t('docs_value'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+    maxVisiblePages: {
+      control: 'number',
+      description: t('docs_value'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+    totalItems: {
+      control: 'number',
+      description: t('docs_value'),
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+    selectPageText: {
+      control: 'text',
+      description: t('docs_content'),
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Page:' },
+        category: 'Component',
+      },
+    },
+    onPageChange: {
+      description: t('docs_click'),
+      table: {
+        type: { summary: '(page: number) => void' },
+        defaultValue: { summary: 'undefined' },
+        category: 'Component',
+      },
+    },
+  },
 };
 
 export default meta;
